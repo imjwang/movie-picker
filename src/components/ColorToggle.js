54cf6c01e56ef4802db6caed6e488c3aa4b0459b
Jeffrey Wang <ngjaywa@gmail.com>
2023-03-05 14:40:33 -0500

    dark/light toggle switch

--- src/components/ColorToggle.js
+++ src/components/ColorToggle.js
@@ -1,6 +1,8 @@
 import { useColorScheme } from "@mui/joy/styles";
-import Button from "@mui/joy/Button";
 import { useState, useEffect } from "react";
+import { Switch } from "@mui/joy";
+import DarkMode from "@mui/icons-material/DarkMode";
+import LightMode from "@mui/icons-material/LightMode";
 
 const ColorToggle = () => {
   const { mode, setMode } = useColorScheme();
@@ -11,17 +13,23 @@ const ColorToggle = () => {
   }, []);
 
   if (!mounted) {
-    // to avoid layout shift, render a placeholder button
-return <Button variant="outlined" color="neutral" sx={{ width: 120 }} />;
+    // to avoid layout shift, render a placeholder switch
+    return <Switch color="primary" />;
   }
   return (
-    <Button
-      variant="outlined"
-      color="neutral"
+    <Switch
+      slotProps={{
+        input: { "aria-label": "Toggle Light/Dark mode" },
+        thumb: {
+          children: mode === "dark" ? <LightMode /> : <DarkMode />,
+        },
+      }}
+      sx={{
+        "--Switch-thumb-size": "24px",
+      }}
+      color="primary"
       onClick={() => setMode(mode === "dark" ? "light" : "dark")}
-    >
-      {mode === "dark" ? "Turn light" : "Turn dark"}
-    </Button>
+    />
   );
 };
 
