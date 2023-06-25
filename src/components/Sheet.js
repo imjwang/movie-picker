508dc8db6e5b3daa5d9ff0200febcb48490d5b22
Jeffrey Wang <ngjaywa@gmail.com>
2023-06-24 22:26:01 -0400

    testing

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -1,7 +1,7 @@
 const Sheet = ({ children, prose }) => {
   return (
     <div
-      className={`p-6 md:p-20 flex flex-col gap-x-2 gap-y-2 bg-inherit ${
+      className={`p-6 md:p-20 flex flex-col gap-x-2 gap-y-2 bg-cream ${
         prose && "prose"
       }`}
     >
