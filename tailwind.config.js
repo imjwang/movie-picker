c2a8f69c6395982fb442f5bfd44e63c05ca0a29a
Jeffrey Wang <ngjaywa@gmail.com>
2023-06-24 22:29:51 -0400

    fix gpt mistakes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        'pastel-blue': '#add8e6',
-        'pastel-red': '#ff6961',
-        'cream': '#fffdd0',
-      }
-    }
-  }
-}
+        "pastel-blue": "#add8e6",
+        "pastel-red": "#ff6961",
+        cream: "#fffdd0",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["bumblebee", "luxury"],
+  },
+};
